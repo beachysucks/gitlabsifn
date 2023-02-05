714b662d5009d97e70aac2c3348b0394d093d370
aaaaaaa <toasterpanic@gmail.com>
2023-02-05 20:29:14 +0000

    Update file 4chan.js

--- clockwork-app/4chan.js
+++ clockwork-app/4chan.js
@@ -2,7 +2,7 @@ function start() {
 
   ////CONFIG////
 
-  var link = "https://holyubofficial.net/uv/service/hvtrs8%2F-wuw%2C4ahcn%2Copg-"; // link to app webpage
+  var link = "https://clockwork-apps.glitch.me/service/gateway?url=https://www.4chan.org/"; // link to app webpage
   var id = "4chan-CW-IFrame"; // a UNIQUE id to identify the frame for your app
   var title = "4chan"; // title shown in navbar
   var uninstallId = "https://redstone-nw.netlify.app/clockwork-app/4chan.js"; // set this to the url location of your script
