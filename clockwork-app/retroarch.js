6b4d735300bf04a92177c2611424976a856f5e13
aaaaaaa <toasterpanic@gmail.com>
2023-02-05 15:53:59 +0000

    Update 2 files
    - /clockwork-app/retroarch.js
    - /clockwork-app/index.html

---
+++ clockwork-app/retroarch.js
@@ -0,0 +1,26 @@
+////CONFIG////
+
+var link = "https://web.libretro.com/"; // link to app webpage
+var id = "Retroarch-CW-IFrame"; // a UNIQUE id to identify the frame for your app
+var title = "RetroArch"; // title shown in navbar
+var uninstallId = "https://redstone-nw.netlify.app/clockwork/retroarch.js"; // set this to the url location of your script
+
+////SCRIPT////
+
+var ExampleLink = document.createElement("a");
+var ExampleIFrame = document.createElement("iframe");
+
+ExampleIFrame.style = "display: none;";
+ExampleIFrame.className = "app";
+ExampleIFrame.id = id; 
+ExampleIFrame.src = "about:blank";
+
+ExampleLink.href = "javascript:openapp('"+ ExampleIFrame.id +"','" + link + "');" ;
+ExampleLink.innerHTML = "Example"; 
+ExampleLink.uninstallId = uninstallId;
+ExampleLink.addEventListener('dblclick', (e) => {
+  uninstallApp(ExampleLink.uninstallId, [ExampleLink,ExampleIFrame]);
+});
+
+document.getElementById("navbar").appendChild(ExampleLink);
+document.getElementById("main").appendChild(ExampleIFrame);
