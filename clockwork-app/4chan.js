15e66ba7ecb5997c28d7afc1cea9f8854823bb02
aaaaaaa <toasterpanic@gmail.com>
2023-02-05 20:24:20 +0000

    Update 2 files
    - /clockwork-app/4chan.js
    - /clockwork-app/index.html

---
+++ clockwork-app/4chan.js
@@ -0,0 +1,32 @@
+function start() {
+
+  ////CONFIG////
+
+  var link = "https://4chan.org/"; // link to app webpage
+  var id = "4chan-CW-IFrame"; // a UNIQUE id to identify the frame for your app
+  var title = "4chan"; // title shown in navbar
+  var uninstallId = "https://redstone-nw.netlify.app/clockwork-app/4chan.js"; // set this to the url location of your script
+  
+  ////SCRIPT////
+  
+  var ExampleLink = document.createElement("a");
+  var ExampleIFrame = document.createElement("iframe");
+  
+  ExampleIFrame.style = "display: none;";
+  ExampleIFrame.className = "app "+id;
+  ExampleIFrame.id = id;
+  ExampleIFrame.src = "about:blank";
+  
+  ExampleLink.href = "javascript:openapp('"+ ExampleIFrame.id +"','" + link + "');" ;
+  ExampleLink.innerHTML = title; 
+  ExampleLink.uninstallId = uninstallId;
+  ExampleLink.className = id; 
+  ExampleLink.addEventListener('dblclick', (e) => {
+    uninstallApp(ExampleLink.uninstallId, ExampleLink.className);
+  });
+  
+  document.getElementById("navbar").appendChild(ExampleLink);
+  document.getElementById("main").appendChild(ExampleIFrame);
+}
+
+start();
