4554a74c7fda86c89925b9e66f9ee09bae6e8868
aaaaaaa <toasterpanic@gmail.com>
2023-02-06 15:24:59 +0000

    Update 2 files
    - /clockwork-app/shadow.js
    - /clockwork-app/index.html

---
+++ clockwork-app/shadow.js
@@ -0,0 +1,32 @@
+function start() {
+
+  ////CONFIG////
+
+  var link = "https://insert-comment-about-daniel.glitch.me/home.html"; // link to app webpage
+  var id = "Shadow-CW-IFrame"; // a UNIQUE id to identify the frame for your app
+  var title = "Shadow"; // title shown in navbar
+  var uninstallId = "https://redstone-nw.netlify.app/clockwork-app/shadow.js"; // set this to the url location of your script
+  
+  ////SCRIPT////
+  
+  var ExampleLink = document.createElement("a");
+  var ExampleIFrame = document.createElement("iframe");
+  
+  ExampleIFrame.style = "display: none;";
+  ExampleIFrame.className = "app "+id;
+  ExampleIFrame.id = id;
+  ExampleIFrame.src = "about:blank";
+  
+  ExampleLink.href = "javascript:openapp('"+ ExampleIFrame.id +"','" + link + "');" ;
+  ExampleLink.innerHTML = title; 
+  ExampleLink.uninstallId = uninstallId;
+  ExampleLink.className = id; 
+  ExampleLink.addEventListener('dblclick', (e) => {
+    uninstallApp(ExampleLink.uninstallId, ExampleLink.className);
+  });
+  
+  document.getElementById("navbar").appendChild(ExampleLink);
+  document.getElementById("main").appendChild(ExampleIFrame);
+}
+
+start();
